00477768af38a540143b7998c1f2937e93ea61b3
bvth <thanh.bui1995@gmail.com>
2018-03-29 22:50:07 +0300

    add gradient bg when button is hovered

--- src/components/button.js
+++ src/components/button.js
@@ -5,15 +5,42 @@ class Button extends React.Component{
     constructor(){
         super();
         this.Click = this.inputNumber.bind(this);
+        this.Over = this.mouseMove.bind(this);
+        this.Clear = this.clearBackground.bind(this);
     }
 
     inputNumber(){
         console.log(this.props.value);
     }
 
+    mouseMove(e){
+        let el = e.target; //select current element
+        let top = el.getBoundingClientRect().top; //get offset top
+        let left = el.getBoundingClientRect().left; //get offset left
+
+
+        let bgColor;
+        if(el.getAttribute("class").includes("operator")){
+            bgColor= "#c7efe5,#64d3b7";
+        }
+        else{
+            bgColor= "#ffffff,#9a9a9a"
+        }
+        el.setAttribute("style","background: radial-gradient(at "+(e.clientX-left)+"px "+(e.clientY-top)+"px,"+bgColor+")!important");
+    }
+
+    clearBackground(e){
+        let el = e.target;
+        el.removeAttribute("style");
+    }
+
     render(){
         return(
-            <div className={"button button_"+this.props.value+" "+this.props.type} onClick={this.Click}  style={{"gridArea":this.props.style}}>{this.props.value}</div>
+            <div className={"button button_"+this.props.value+" "+this.props.type} 
+                    onClick={this.Over}  
+                    onMouseMove={this.Over}
+                    onMouseOut={this.Clear}
+                    style={{"gridArea":this.props.style}} >{this.props.value}</div>
         )
     }
 }
